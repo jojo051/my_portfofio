4341cb6006266a55fc9a87cc4f581367e843c944
jojo051 <youpi051@yahoo.fr>
2020-07-22 14:22:25 +0200

    init serveur

---
+++ back/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.get('/', (request, response) => {
+  response.send('Bienvenue sur Express');
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    throw new Error('Something bad happened...');
+  }
+
+  console.log(`Server is listening on ${port}`);
+});
