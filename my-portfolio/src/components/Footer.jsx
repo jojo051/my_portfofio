6bf78d001e46e6c74b24cc9bac4935c78cefe107
jojo051 <youpi051@yahoo.fr>
2020-07-23 10:03:15 +0200

    refactor(footer):link in footer

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <>
-    hello function flecher
+    <a href="https://www.linkedin.com/in/joel-rodier-6280b41a4/"
+      title="Lien Linkedin"
+      target="_blank">Lien Linkedin</a>
     </>
   );
 }
