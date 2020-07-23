5ec9064a49614d64ca9c5576ed14a7023561de0c
jojo051 <youpi051@yahoo.fr>
2020-07-23 09:46:58 +0200

    refactor(Home):add tetxte

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-    hello function flecher
+      <div> 
+      <h1>Bonjour</h1>
+      <h1> Je me presente</h1>
+      <p> moi, c'est joel, ... lorem ipsum</p>
+      <p> lorem ipsum</p>
+      <img src="" alt="" />
+      <h2>Bon passons aux choses serieuses ...</h2>
+      <p>Voici quelques uns des projets que je suis fier de vous presenter :</p>
+  
+      </div>
     </>
   );
 }
