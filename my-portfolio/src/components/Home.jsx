57abd36159a547888fa93cb668fb367180f8fc05
jojo051 <youpi051@yahoo.fr>
2020-07-23 21:06:11 +0200

    refector(Project)add buton

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Footer from './Footer';
-import {
-  Link,
-} from 'react-router-dom';
 import ContactMe from './ContactMe';
 import MesProjets from './MesProjets';
 import zoomjojo from '../image/zoomjojo.png';
@@ -14,17 +11,11 @@ const Home = () => {
         <h1>Bonjour</h1>
         <h2> Je me présente</h2>
         <p> moi, c'est joel, ... lorem ipsum</p>
-        <p> lorem ipsum</p>
+        <p> Je suis développeur Web junior à la recherche d'un stage, extrêmement motivé pour développer constamment mes compétences et évoluer professionnellement</p>
         <img src={zoomjojo} alt="" />
         <h2>Bon passons aux choses serieuses ...</h2>
         <p>Voici quelques uns des projets que je suis fier de vous presenter :</p>
           <MesProjets />
-
-          <Link className="BtnLink" to="/projet">
-            voir le projet
-            <img src="" alt="" className="BtnImg" />
-          </Link>
-
         <h2>Envie de me contacter ?</h2>
           <ContactMe />
       </div>
--- my-portfolio/src/components/Projet.jsx
+++ my-portfolio/src/components/Projet.jsx
@@ -2,20 +2,26 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import {
+  Link,
   useParams,
 } from 'react-router-dom';
 import Footer from './Footer';
 import ContactMe from './ContactMe';
 
 const Projet = () => {
+
   const { projectId } = useParams();
   const [project, setProject] = useState([]);
+  
+
   useEffect (() => {
     Axios.get(`http://localhost:8000/projects/${projectId}`,)
       .then((response) => {
         setProject(response.data);
       });
-  }, []);
+  }, [projectId]);
+    
+  
 
   return (
     <>
@@ -32,6 +38,14 @@ const Projet = () => {
             <div>
             {project.lien}
             </div>
+          </div>
+          <div> 
+            <button type="button" onClick={projectId -1} >projet précédent</button>
+            <Link className="BtnLink" to="/home">
+              home
+              <img src="" alt="" className="" />
+            </Link>
+            <button type="button" onClick={projectId +1}>projet suivant</button>
           </div>
         <div><ContactMe /></div>
         <div><Footer /></div>
