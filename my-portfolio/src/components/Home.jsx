4e67ff2349e2f41f248cfdfd6b5c93a6602d882c
jojo051 <youpi051@yahoo.fr>
2020-07-23 10:26:11 +0200

    build(contact):add component contact

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -6,12 +6,15 @@ const Home = () => {
     <>
       <div> 
       <h1>Bonjour</h1>
-      <h1> Je me presente</h1>
+      <h2> Je me presente</h2>
       <p> moi, c'est joel, ... lorem ipsum</p>
       <p> lorem ipsum</p>
       <img src="" alt="" />
       <h2>Bon passons aux choses serieuses ...</h2>
       <p>Voici quelques uns des projets que je suis fier de vous presenter :</p>
+      <projets></projets>
+      <h2>Envie de me cotacter ?</h2>
+      <button type="button" >Envoyer un Email</button>
       </div>
       <Footer />
     </>
