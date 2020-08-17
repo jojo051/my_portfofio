ae9d2912f6580395f8171839263f87ec0dd02c3e
jojo051 <youpi051@yahoo.fr>
2020-08-17 09:42:46 +0200

    refactor(home): add css

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -6,22 +6,22 @@ import zoomjojo from '../image/zoomjojo.png';
 
 const Home = () => {
   return (
-      <section className="home"> 
+      <section className="flex flex-col justify-center text-center"
+      > 
         <div>
-          <h1 className="text-6xl text-white bg-blue-500 h-40 flex justify-center items-center" >Bonjour</h1>
-          <div className="flex flex-col">
-            <h2 className="flex justify-center"> Je me présente</h2>
-            <p className="flex justify-center"> moi, c'est joel, ... lorem ipsum</p>
-            <p className="flex justify-center" > Je suis développeur Web junior à la recherche d'un stage, extrêmement motivé pour développer constamment mes compétences et évoluer professionnellement</p>
+          <h1 className="text-6xl text-white bg-blue-500 h-45 flex justify-center items-center" >Bonjour</h1>
+          <div className="">
+            <h2 className="">Je me présente</h2>
+            <p className=""> moi, c'est joel,</p>
+            <p className="" > Je suis développeur Web junior à la recherche d'un stage, extrêmement motivé pour développer constamment mes compétences et évoluer professionnellement</p>
           </div> 
             <div className="flex justify-center"><img className="max-w-xl" src={zoomjojo} alt="my head" /></div>
-            <h1 className="text-6xl text-white bg-blue-500 h-40 flex justify-center items-center">Bon passons aux choses serieuses ...</h1>
+            <h1 className="text-6xl text-white bg-blue-500 h-45 text-center flex items-center">Bon passons aux choses serieuses ...</h1>
             <p>Voici quelques uns des projets que je suis fier de vous presenter :</p>
           <div className="flex justify-center">
             <MesProjets />
           </div>
           <div className="">
-            
               <ContactMe />
           </div>
           <Footer />
